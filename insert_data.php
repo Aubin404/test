26029509ae666c8b49c76b25c29d75fc22694d1c
Aubin404 <139377421+Aubin404@users.noreply.github.com>
2023-08-30 19:05:55 +0200

    Update insert_data.php

--- insert_data.php
+++ insert_data.php
@@ -1,11 +1,33 @@
 <?php
+// Connexion à la base de données
+$bdd = new SQLite3('db.db');
+
+if (!$bdd) {
+    die("Erreur de connexion à la base de données");
+}
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $password = $_POST["password"];
     $email = $_POST["email"];
 
-    // Exécute le script Python avec les informations en tant qu'arguments
-    $output = shell_exec("python db.py \"$password\" \"$email\"");
+    // Création d'une table si elle n'existe pas déjà
+    $bdd->exec('
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY,
+            email INTEGER TEXT,
+            password INTEGER TEXT
+        )
+    ');
+
+    // Insertion de données
+    $stmt = $bdd->prepare('INSERT INTO users (email, password) VALUES (:email, :password)');
+    $stmt->bindParam(':email', $email, SQLITE3_TEXT);
+    $stmt->bindParam(':password', $password, SQLITE3_TEXT);
+    $stmt->execute();
     
-    echo $output;
+    echo "Données insérées avec succès dans la base de données.";
+
+    // Fermeture de la connexion à la base de données
+    $bdd->close();
 }
 ?>
